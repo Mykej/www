ae62d7aed72786ad79af7501a465528d96e492e0
Mykael Thompson <mekrfz23_23@hotmail.com>
2021-01-31 01:33:35 -0600

    site.php

---
+++ site.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset = "utf-8"/>
+        <title></title>
+    </head>
+    <body>
+        <?php 
+            echo("<h1>Hello World!</h1>");
+            echo('<hr>');
+            echo('<p>This is my site</p>');
+
+
+            $name = "Mike";
+            $age = 30;
+            echo"there once was a man named $name <br>";
+            echo"He was $age years old <br>";
+            $name = "Tom";
+            echo"He really liked the name $name <br>";
+            echo"But didn't like being $age <br>";
+
+
+            $phrase = "To be or not to be";
+            $int = -23;
+            $float = 23.43;
+            $isMale = false;
+            $arr = [1,2,3,4];
+            null;
+            echo ($phrase);
+        ?>
+
+
+    </body>
+</html>
